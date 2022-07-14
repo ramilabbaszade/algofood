62449d07bbeed91927573dfa6ff2cabd15a67fe1
ramilabbaszade <abbasovramil65@gmail.com>
2022-07-14 04:32:18 +0400

    remove: unused react-router-dom package

--- src/main.jsx
+++ src/main.jsx
@@ -3,18 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "@/App";
 import store from "@/redux/store";
 
-import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
 import "@/assets/scss/main.scss";
-import 'rsuite/dist/rsuite.min.css';
+import "rsuite/dist/rsuite.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <App />
     </Provider>
   </React.StrictMode>
 );
